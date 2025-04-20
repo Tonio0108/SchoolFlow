dcdce31594ca9be5606ca670e9d93de61b0bce95
rija062004 <rija.fam@gmail.com>
2025-04-20 21:34:53 +0300

    Code pour le CRUD de la table professeur, et la mis en place de ses routes, tout est clean !

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const salleRouter = require('./routes/salle.routes');
+const professeurRoutes = require('./routes/professeur.routes');
 require('dotenv').config();
 
 const app = express();
@@ -12,5 +13,7 @@ app.get('/api/hello', (req, res) => {
 });
 
 app.use('/salle', salleRouter)
+app.use('/professeur', professeurRoutes)
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
